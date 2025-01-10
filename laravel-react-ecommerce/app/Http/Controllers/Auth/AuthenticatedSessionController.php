582880581ea22686693bf9c9dce2701c0a8d6752
sergeycin <67867645+sergeycin@users.noreply.github.com>
2025-01-10 17:06:32 +0100

    Admin panel filament added

--- laravel-react-ecommerce/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ laravel-react-ecommerce/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\RolesEnum;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,13 +28,25 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request): \Symfony\Component\HttpFoundation\Response
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        /**
+          @var User $user
+         */
+        $user = Auth::user();
+        $route = "/";
+        if($user->hasAnyRole([RolesEnum::Admin, RolesEnum::Vendor])){
+            return Inertia::location(route('filament.admin.pages.dashboard'));
+        } else{
+            $route = route('dashboard', absolute:false);
+        }
+
+
+        return redirect()->intended($route);
     }
 
     /**
